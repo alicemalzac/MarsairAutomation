f7e1a2eb64d12ea023e584181279565e5df127cd
Alice <alicelinsc.malzac@gmail.com>
2025-05-05 13:23:00 -0300

    updating test cases numbers

--- cypress/e2e/verify.cy.js
+++ cypress/e2e/verify.cy.js
@@ -9,7 +9,7 @@ describe('Verify Search Selection Test Cases', () => {
       HomePage.visit();
     })
 
-  it('TC001: should search for a valid date range (currently results in no seats)', () => {
+  it('TC001: Should search for a valid date range', () => {
     HomePage.selectDepartingMonth('0'); // July
     HomePage.selectReturningMonth('2'); // July (next year)
     HomePage.clickSearch();
@@ -18,64 +18,74 @@ describe('Verify Search Selection Test Cases', () => {
       expect(text.includes(successMsg) || text.includes(noSeatsMsg)).to.be.true;
     });
     HomePage.goToHomePage();
-});
-
-    it('TC005: Should display error for return date < 1 year after departure (July to Dec)', () => {
-      HomePage.selectDepartingMonth('0'); // July
-      HomePage.selectReturningMonth('1'); // December
-      HomePage.clickSearch();
-      HomePage.verifyMessage(invalidScheduleMsg);
-      HomePage.clickBack();
-    });
-
-    it('TC006: should display error for return date < 1 year after departure (July next to Dec next)', () => {
-      HomePage.selectDepartingMonth('2'); // July (next year)
-      HomePage.selectReturningMonth('3'); // December (next year)
-      HomePage.clickSearch();
-      HomePage.verifyMessage(invalidScheduleMsg);
-    });
-
-    it('TC007: should display error for return date < 1 year after departure (July two to Dec two)', () => {
-      HomePage.selectDepartingMonth('4'); // July (two years from now)
-      HomePage.selectReturningMonth('5'); // December (two years from now)
-      HomePage.clickSearch();
-      HomePage.verifyMessage(invalidScheduleMsg);
-    });
-
-    it('TC008: should display error for same departure and return month (July)', () => {
-      HomePage.selectDepartingMonth('0'); // July
-      HomePage.selectReturningMonth('0'); // July
-      HomePage.clickSearch();
-      HomePage.verifyMessage(invalidScheduleMsg);
-    });
-
-    it('TC009: should display error for same departure and return month (December)', () => {
-      HomePage.selectDepartingMonth('1'); // December
-      HomePage.selectReturningMonth('1'); // December
-      HomePage.clickSearch();
-      HomePage.verifyMessage(invalidScheduleMsg); // bug found 
-    });
-
-    it('TC014: should display error for return date before departure date', () => {
-      HomePage.selectDepartingMonth('5'); // December (two years from now)
-      HomePage.selectReturningMonth('3'); // December (next year)
-      HomePage.clickSearch();
-      HomePage.verifyMessage(invalidScheduleMsg); // bug found 
   });
 
-  it('TC015: should display error when departure date is not selected', () => {
-      HomePage.selectReturningMonth('2'); // July (next year)
-      HomePage.clickSearch();
-      HomePage.verifyMessage(invalidScheduleMsg); // bug found 
+  it('TC002: Should search for a valid date range', () => {
+    HomePage.selectDepartingMonth('3'); // December (next year)
+    HomePage.selectReturningMonth('5'); // December (two years from now)
+    HomePage.clickSearch();
+    cy.get('#content').should(($content) => {
+      const text = $content.text();
+      expect(text.includes(successMsg) || text.includes(noSeatsMsg)).to.be.true;
+    });
+    HomePage.goToHomePage();
   });
 
-  it('TC016: should display error when return date is not selected', () => {
-      HomePage.selectDepartingMonth('0'); // July
-      HomePage.clickSearch();
-      HomePage.verifyMessage(invalidScheduleMsg); // bug found 
+  it('TC003: Should display error for return date < 1 year after departure (July to Dec)', () => {
+    HomePage.selectDepartingMonth('0'); // July
+    HomePage.selectReturningMonth('1'); // December
+    HomePage.clickSearch();
+    HomePage.verifyMessage(invalidScheduleMsg);
+    HomePage.clickBack();
   });
-  it('TC017: should display "no seats" (incorrectly) when both dates are not selected', () => {
-      HomePage.clickSearch();
-      HomePage.verifyMessage(invalidScheduleMsg); // bug found 
+
+  it('TC004: Should display error for return date < 1 year after departure (July two to Dec two)', () => {
+    HomePage.selectDepartingMonth('4'); // July (two years from now)
+    HomePage.selectReturningMonth('5'); // December (two years from now)
+    HomePage.clickSearch();
+    HomePage.verifyMessage(invalidScheduleMsg);
+  });
+
+  it('TC005: Should display error for same departure and return month (July)', () => {
+    HomePage.selectDepartingMonth('0'); // July
+    HomePage.selectReturningMonth('0'); // July
+    HomePage.clickSearch();
+    HomePage.verifyMessage(invalidScheduleMsg);
+    cy.log('Expected Behavior: Should display a message: Unfortunately, this schedule is not possible. Please try again.'); // bug found 
+  });
+
+  it('TC006: Should display error for same departure and return month (December)', () => {
+    HomePage.selectDepartingMonth('5'); // December (two years from now)
+    HomePage.selectReturningMonth('5'); // December (two years from now)
+    HomePage.clickSearch();
+    HomePage.verifyMessage(invalidScheduleMsg);
+    cy.log('Expected Behavior: Should display a message: Unfortunately, this schedule is not possible. Please try again.'); // bug found 
+  });
+
+  it('TC007: Should display error for return date before departure date', () => {
+    HomePage.selectDepartingMonth('5'); // December (two years from now)
+    HomePage.selectReturningMonth('3'); // December (next year)
+    HomePage.clickSearch();
+    HomePage.verifyMessage(invalidScheduleMsg);
+    cy.log('Expected Behavior: Should display a message: Unfortunately, this schedule is not possible. Please try again.'); // bug found  
+  });
+
+  it('TC008: Should display error when departure date is not selected', () => {
+    HomePage.selectReturningMonth('2'); // July (next year)
+    HomePage.clickSearch();
+    HomePage.verifyMessage(invalidScheduleMsg); 
+    cy.log('Expected Behavior: Should display a message: Unfortunately, this schedule is not possible. Please try again.'); // bug found  
+  });
+
+  it('TC009: should display error when return date is not selected', () => {
+    HomePage.selectDepartingMonth('0'); // July
+    HomePage.clickSearch();
+    HomePage.verifyMessage(invalidScheduleMsg); 
+    cy.log('Expected Behavior: Should display a message: Unfortunately, this schedule is not possible. Please try again.'); // bug found 
+  });
+  it('TC010: Should display show error when both departing and returning date is not selected', () => {
+    HomePage.clickSearch();
+    HomePage.verifyMessage(invalidScheduleMsg); 
+    cy.log('Expected Behavior: Should display a message: Unfortunately, this schedule is not possible. Please try again.'); // bug found 
   });
 })
